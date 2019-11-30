1c6324d7d896b15a61e9dc50467284f2e0a53589
Diogo Pereira <diogoalexp@gmail.com>
2019-11-30 18:12:53 -0300

    core functions

---
+++ src/containers/Home/Home.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+
+import Input from '../../components/UI/Input/Input';
+import Button from '../../components/UI/Button/Button';
+import Spinner from '../../components/UI/Spinner/Spinner';
+// import classes from './Auth.module.css';
+import * as actions from '../../store/actions/index';
+import { updateObject, checkValidity } from '../../shared/utility';
+
+class Home extends Component {
+
+    render () {
+        return (
+            <div>
+                Home
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        // loading: state.auth.loading,
+        // error: state.auth.error,
+        // isAuthenticated: state.auth.token !== null,
+        // buildingBurger: state.burgerBuilder.building,
+        // authRedirectPath: state.auth.authRedirectPath
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        // onAuth: ( email, password, isSignup ) => dispatch( actions.auth( email, password, isSignup ) ),
+        // onSetAuthRedirectPath: () => dispatch( actions.setAuthRedirectPath( '/' ) )
+    };
+};
+
+export default connect( mapStateToProps, mapDispatchToProps )( Home );
