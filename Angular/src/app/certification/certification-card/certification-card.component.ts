5a352b76234c4deebed7a47907ff839a2c1d8c4d
Diogo Pereira <diogo.pereira@ezoqc.com>
2025-05-19 12:20:00 -0400

    angula: certifications data

--- Angular/src/app/certification/certification-card/certification-card.component.ts
+++ Angular/src/app/certification/certification-card/certification-card.component.ts
@@ -1,12 +1,17 @@
 import { Component, input } from '@angular/core';
 import { Certification } from '../certification.model';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-certification-card',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './certification-card.component.html',
   styleUrl: './certification-card.component.css'
 })
 export class CertificationCardComponent {
   certification = input.required<Certification>();
+
+  onClickExternalLink = () =>{
+    window.open(this.certification().url , '_blank');
+  }
 }
